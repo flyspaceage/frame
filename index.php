93076863824978085c223d2a3531f9beb4ebcdac
Joseph Layden <layden.joseph@gmail.com>
2017-06-09 11:31:43 -0500

    something do

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
 		<meta charset="UTF-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"/>
 
-		<title>Frame | Atrium</title>
+		<title>Flip Bits</title>
 		<meta name="description" content="" />
 		<meta name="keywords" content="" />
 		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no, minimal-ui" />
